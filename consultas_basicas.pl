6acda6648cf96d31aa2994baee95c5e91528f8d1
henriquelaia <152277818+henriquelaia@users.noreply.github.com>
2025-06-05 16:48:50 +0100

    Delete consultas_basicas.pl

--- consultas_basicas.pl
+++
@@ -1,3 +0,0 @@
-?- melhor_equipa_por_pontos(2001, 'English Premier League', Equipa).
-?- melhor_ataque(2001, 'English Premier League', Equipa).
-?- melhor_defesa(2001, 'English Premier League', Equipa).
